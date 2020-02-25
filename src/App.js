fa7325557c5c4fb4fbe2e6a23ca8f12ae69966f7
perade <perade0530@gmail.com>
2020-02-26 00:28:57 +0900

    2.1 Reusable Components with JSX + Props 실습

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import Potato from './Potato';
+
+function Food ({ fav }) {
+return <h1>I love { fav }</h1>;
+}
 
 function App () {
   return (
     <div>
       <h1>Hello!!</h1>
-      <Potato />
+      <Food
+        fav="kimchi"
+      />
+      <Food
+        fav="ramen"
+      />
+      <Food
+        fav="bulgogi"
+      />
     </div>
   );
 }
