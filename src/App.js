2eaec85808ab0db19650fc19a1db84f80dec88e6
perade <perade0530@gmail.com>
2020-02-26 01:39:05 +0900

    2.2 Dynamic Component Generation 실습

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React from 'react';
 
-function Food ({ fav }) {
-return <h1>I love { fav }</h1>;
+function Food ({ name, image }) {
+  return (
+    <div>
+      <h2>I love { name }</h2>
+      <img src={ image } />
+    </div>
+  );
 }
+
+const foods = [
+  {
+    name: 'kimchi',
+    image: 'https://www.maangchi.com/wp-content/uploads/2014/06/whole-cabbage-kimchi.jpg'
+  },
+  {
+    name: 'bulgogi',
+    image: 'https://damndelicious.net/wp-content/uploads/2019/04/Korean-Beef-BulgogiIMG_9246.jpg'
+  },
+  {
+    name: 'ramen',
+    image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/190208-delish-ramen-horizontal-093-1550096715.jpg?crop=1xw:0.7496251874062968xh;center,top&resize=1200:*'
+  }
+];
 
 function App () {
   return (
     <div>
-      <h1>Hello!!</h1>
-      <Food
-        fav="kimchi"
-      />
-      <Food
-        fav="ramen"
-      />
-      <Food
-        fav="bulgogi"
-      />
+      {foods.map(dish => <Food name={dish.name} image={dish.image} />)}
     </div>
   );
 }
