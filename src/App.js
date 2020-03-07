29cb45ebbfaedcbdbadc8a4394b18af7dd173e97
perade <perade0530@gmail.com>
2020-03-07 23:43:18 +0900

    3.0 Class Components and State 실습

--- src/App.js
+++ src/App.js
@@ -1,49 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Food ({ name, image, rating }) {
-  return (
-    <div>
-      <h2>I love { name }</h2>
-      <h4>{rating} / 5</h4>
-      <img src={ image } alt={name} />
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    count: 0
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
+  add = () => {
+    console.log('add');
+  };
 
-const foods = [
-  {
-    id: 1,
-    name: 'kimchi',
-    image: 'https://www.maangchi.com/wp-content/uploads/2014/06/whole-cabbage-kimchi.jpg',
-    rating: 5
-  },
-  {
-    id: 2,
-    name: 'bulgogi',
-    image: 'https://damndelicious.net/wp-content/uploads/2019/04/Korean-Beef-BulgogiIMG_9246.jpg',
-    rating: 4
-  },
-  {
-    id: 3,
-    name: 'ramen',
-    image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/190208-delish-ramen-horizontal-093-1550096715.jpg?crop=1xw:0.7496251874062968xh;center,top&resize=1200:*',
-    rating: 3
+  minus = () => {
+    console.log('minus')
+  };
+
+  render () {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
-];
-
-function App () {
-  return (
-    <div>
-      {foods.map(dish => <Food key={dish.id} name={dish.name} image={dish.image} rating={dish.rating} />)}
-    </div>
-  );
 }
 
 export default App;
