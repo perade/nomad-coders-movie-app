e07db1454072248b7cc6e336c7e2f8163fc7732d
perade <perade0530@gmail.com>
2020-03-13 00:28:11 +0900

    6.3 Sharing Props Between Routes 실습

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
 function Movie ({ id, year, title, summary, poster, genres }) {
   return (
-    <div className="movie">
+    <Link
+      to={{
+        pathname: '/movie-detail',
+        state: {
+          year,
+          title,
+          summary,
+          poster,
+          genres
+        }
+      }}
+    >
+      <div className="movie">
       <img src={poster} alt={title} title={title} />
       <div className="movie-data">
         <h3 className="movie-title">{title}</h3>
@@ -15,6 +28,7 @@ function Movie ({ id, year, title, summary, poster, genres }) {
         <p className="movie-summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
+    </Link>
   );
 };
 
