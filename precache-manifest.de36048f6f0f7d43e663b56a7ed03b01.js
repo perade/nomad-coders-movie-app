3f5b7573ff50238b6f65ab924455044a0218f1c4
perade <perade0530@gmail.com>
2020-03-12 23:26:35 +0900

    Updates

---
+++ precache-manifest.de36048f6f0f7d43e663b56a7ed03b01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c9d315e3001a17e830d3ff2b469402",
+    "url": "/nomad-coders-movie-app/index.html"
+  },
+  {
+    "revision": "bd25633f53df6ccb69ff",
+    "url": "/nomad-coders-movie-app/static/css/main.69ddf344.chunk.css"
+  },
+  {
+    "revision": "38fbca0e125b49512f1e",
+    "url": "/nomad-coders-movie-app/static/js/2.b10607fd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/nomad-coders-movie-app/static/js/2.b10607fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd25633f53df6ccb69ff",
+    "url": "/nomad-coders-movie-app/static/js/main.e781f95b.chunk.js"
+  },
+  {
+    "revision": "f73f91e50f2cdf4c4ac9",
+    "url": "/nomad-coders-movie-app/static/js/runtime-main.729532a4.js"
+  }
+]);
